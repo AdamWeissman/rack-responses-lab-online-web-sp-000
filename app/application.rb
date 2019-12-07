e0cf4a77f492ace20995f2cdda367d51c3d6114c
Adam Weissman <adam@comedytragedyepic.com>
2019-12-07 11:33:11 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -5,7 +5,7 @@ class Application
     
     less_than_12 = Time.now.hour < 12
     
-    (less_than_12) resp.write "Good Morning!" : resp.write "Good Afternoon!"
+    (less_than_12)? resp.write "Good Morning!" : resp.write "Good Afternoon!"
  
  
     #current_time = Time.now
