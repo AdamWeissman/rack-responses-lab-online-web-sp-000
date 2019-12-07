19ebe6a1b4d14894baf3a9a37393ec885da21212
Adam Weissman <adam@comedytragedyepic.com>
2019-12-07 11:31:30 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -2,15 +2,18 @@ class Application
  
   def call(env)
     resp = Rack::Response.new
+    
+    (Time.now < 12)? (resp.write "Good Morning!") : (resp.write "Good Afternoon!")
  
-    current_time = Time.now
-    time = current_time.hour
  
-    if time < 12
-      resp.write "Good Morning!"
-    else
-      resp.write "Good Afternoon!"
-    end
+    #current_time = Time.now
+    #time = current_time.hour
+ 
+    #if time < 12
+    #  resp.write "Good Morning!"
+    #else
+    #  resp.write "Good Afternoon!"
+    #end
  
     resp.finish
   end
