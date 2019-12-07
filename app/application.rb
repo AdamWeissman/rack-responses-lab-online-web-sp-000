1d793422c01fc49950b676be31304a39e437b0fe
Adam Weissman <adam@comedytragedyepic.com>
2019-12-07 11:25:23 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -1,14 +1,11 @@
   def call(env)
     resp = Rack::Response.new
  
-    num_1 = Kernel.rand(1..20)
-    num_2 = Kernel.rand(1..20)
-    num_3 = Kernel.rand(1..20)
- 
-    if num_1==num_2 && num_2==num_3
-      resp.write "You Win"
-    else
-      resp.write "You Lose"
+    now = Time.now
+    if now.hour >=12
+      resp.write "Good Afternoon!"
+    elsif now.hour <= 12
+      resp.write "Good Morning!"
     end
  
     resp.finish
